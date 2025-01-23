378ebad7812372cc070ff424142c97601bb111e3
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2025-01-23 16:18:56 +0100

    better naming on functions for clearity

--- js/script.js
+++ js/script.js
@@ -1,24 +1,42 @@
 async function fetchPosts() {
-  const response = await fetch("http://localhost:5005/posts");
-  const data = await response.json(); // Fetch the entire response object
-  const posts = data.result; // Extract the posts array from the result property
+  try {
+    const response = await fetch("http://localhost:5005/posts");
+    const data = await response.json(); // Fetch the entire response object
+    const posts = data.result; // Extract the posts array from the result property
+    return posts;
+  } catch (error) {
+    console.error(
+      "There was an error with fetching post with fetchPost",
+      error
+    );
+    return [];
+  }
+};
 
+function displayPosts(singlePosts) {
   const postsDiv = document.getElementById("posts");
-  posts.forEach((post) => {
-    const postHtml = `
+  singlePosts.forEach((postContainer) => {
+    const postHtmlContent = `
         <div>
-          <h2>${post.title}</h2>
-          <p>${post.content}</p>
-          <p>By: ${post.publisher_name}</p>
-          ${
-            post.image_path
-              ? `<img src="http://localhost:5005${post.image_path}" alt="${post.title}" />`
-              : ""
-          }
+            <h2>${postContainer.title}</h2>
+            <p>${postContainer.content}</p>
+            <p>By: ${postContainer.publisher_name}</p>
+            ${
+              postContainer.image_path
+                ? `<img src="http://localhost:5005${postContainer.image_path}" alt="${postContainer.title}" />`
+                : ""
+            }
         </div>
       `;
-    postsDiv.innerHTML += postHtml;
+    postsDiv.innerHTML += postHtmlContent;
   });
-}
+};
 
 fetchPosts();
+
+async function loadPostsInRightOrder() {
+  const waitOnFetchPosts = await fetchPosts();
+  displayPosts(waitOnFetchPosts);
+};
+
+loadPostsInRightOrder();
